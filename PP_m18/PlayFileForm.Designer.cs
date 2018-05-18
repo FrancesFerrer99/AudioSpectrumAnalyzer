42f94bfa47a145a1c6ef4add358b621659d5756a
Frances <frances.ferrer1999@gmail.com>
2018-05-18 13:58:12 +0200

    aggiunta cartella mancante PP_m18

---
+++ PP_m18/PlayFileForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace PP_m18
+{
+    partial class PlayFileForm
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.buttonPause = new System.Windows.Forms.Button();
+            this.openAudioFile = new System.Windows.Forms.OpenFileDialog();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.pictureBoxWave = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWave)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(13, 13);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(150, 25);
+            this.buttonOpen.TabIndex = 0;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Enabled = false;
+            this.buttonPlay.Location = new System.Drawing.Point(12, 44);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(151, 25);
+            this.buttonPlay.TabIndex = 1;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // buttonPause
+            // 
+            this.buttonPause.Enabled = false;
+            this.buttonPause.Location = new System.Drawing.Point(169, 13);
+            this.buttonPause.Name = "buttonPause";
+            this.buttonPause.Size = new System.Drawing.Size(151, 25);
+            this.buttonPause.TabIndex = 2;
+            this.buttonPause.Text = "Pause";
+            this.buttonPause.UseVisualStyleBackColor = true;
+            this.buttonPause.Click += new System.EventHandler(this.buttonPause_Click);
+            // 
+            // openAudioFile
+            // 
+            this.openAudioFile.FileName = "openAudioFile";
+            this.openAudioFile.Filter = "MP3 audio file (*.mp3)|*.mp3|Lossless audio file (*.wav)|*.wav";
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Location = new System.Drawing.Point(169, 44);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(151, 25);
+            this.buttonStop.TabIndex = 3;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // pictureBoxWave
+            // 
+            this.pictureBoxWave.Location = new System.Drawing.Point(13, 75);
+            this.pictureBoxWave.Name = "pictureBoxWave";
+            this.pictureBoxWave.Size = new System.Drawing.Size(559, 325);
+            this.pictureBoxWave.TabIndex = 4;
+            this.pictureBoxWave.TabStop = false;
+            // 
+            // PlayFileForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 412);
+            this.Controls.Add(this.pictureBoxWave);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonPause);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.buttonOpen);
+            this.Name = "PlayFileForm";
+            this.Text = "Play an audio file";
+            this.Load += new System.EventHandler(this.PlayFileForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWave)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonPause;
+        private System.Windows.Forms.OpenFileDialog openAudioFile;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.PictureBox pictureBoxWave;
+    }
+}
+
---
+++ PP_m18/PlayFileForm.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Un4seen.Bass;
+using Un4seen.Bass.Misc;
+using System.Runtime;
+using System.Runtime.InteropServices;
+
+namespace PP_m18
+{
+    public partial class PlayFileForm : Form
+    {
+        private string fileName;
+        private int streamHandle;
+        private BASS_FILEPROCS soundStream;
+        private FileStream fileStream;
+
+        private int waveStreamHandle;
+        private WaveForm waveForm;
+
+        public PlayFileForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            openAudioFile.FileName = String.Empty;
+            if (DialogResult.OK == openAudioFile.ShowDialog(this))
+            {
+                if (File.Exists(openAudioFile.FileName))
+                {
+                    fileName = openAudioFile.FileName;
+                    buttonPlay.Enabled = true;
+                    buttonOpen.Enabled = true;
+                    buttonPause.Enabled = false;
+                    buttonStop.Enabled = false;
+                }
+                else
+                {
+                    fileName = String.Empty;
+                    buttonOpen.Enabled = true;
+                    buttonPlay.Enabled = false;
+                    buttonPause.Enabled = false;
+                    buttonStop.Enabled = false;
+                }
+            }
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            // svuoto sempre lo stream prima di crearne uno nuovo
+            Bass.BASS_StreamFree(streamHandle);
+            Bass.BASS_StreamFree(waveStreamHandle);
+            // controllo che il file di cui creare lo stream esista
+            if (fileName != String.Empty)
+            {
+                // apro il file
+                fileStream = File.OpenRead(fileName);
+                // creo un handle per lo streaming dal file
+                //streamHandle = Bass.BASS_StreamCreateFileUser(BASSStreamSystem.STREAMFILE_NOBUFFER, BASSFlag.BASS_DEFAULT | BASSFlag.BASS_MUSIC_PRESCAN, soundStream, IntPtr.Zero);
+                streamHandle = Bass.BASS_StreamCreateFile(fileName,0,fileStream.Length, BASSFlag.BASS_DEFAULT);
+
+                waveForm = new WaveForm(fileName, new WAVEFORMPROC(WaveChunkLoad), null);
+                waveForm.FrameResolution = 0.01f;
+                waveForm.RenderStartRecording(streamHandle, 5, 5);
+                waveForm.CallbackFrequency = 5;
+
+                if (streamHandle != 0 && Bass.BASS_ChannelPlay(streamHandle, false))
+                {
+                    buttonPause.Enabled = true;
+                    buttonStop.Enabled = true;
+                    buttonPlay.Enabled = false;
+                    buttonOpen.Enabled = false;
+                }
+                else
+                {
+                    buttonOpen.Enabled = true;
+                    buttonPlay.Enabled = false;
+                    buttonPause.Enabled = false;
+                    buttonStop.Enabled = false;
+                }
+
+            }
+        }
+
+        int curFrame = -1;
+        private void WaveChunkLoad(int framesDone, int framesTotal, TimeSpan elapsedTime, bool finished)
+        {
+            //throw new NotImplementedException();
+            //MessageBox.Show(framesDone + Environment.NewLine + elapsedTime);
+            //pictureBoxWave.BackgroundImage = waveForm.CreateBitmap(pictureBoxWave.Width, pictureBoxWave.Height, curFrame, framesTotal, true);
+            curFrame = framesDone;
+            DrawWave();
+        }
+        
+
+        private void DrawWave()
+        {
+            pictureBoxWave.BackgroundImage = waveForm.CreateBitmap(pictureBoxWave.Width, pictureBoxWave.Height, -1, -1, true);
+        }
+
+        private void buttonPause_Click(object sender, EventArgs e)
+        {
+            // metto in pausa la riproduzione
+            if (streamHandle != 0 && Bass.BASS_ChannelPause(streamHandle))
+            {
+                buttonStop.Enabled = true;
+                buttonPlay.Enabled = true;
+                buttonOpen.Enabled = false;
+                buttonPause.Enabled = false;
+            }
+            else
+            {
+                buttonPause.Enabled = true;
+                buttonStop.Enabled = true;
+                buttonOpen.Enabled = false;
+                buttonPlay.Enabled = false;
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            // stoppo la riproduzione
+            if (streamHandle != 0 && Bass.BASS_ChannelStop(streamHandle))
+            {
+                //waveForm.RenderStop(); <-- da spostare!
+                buttonPlay.Enabled = true;
+                buttonOpen.Enabled = true;
+                buttonPause.Enabled = false;
+                buttonStop.Enabled = false;
+                
+            }
+            else
+            {
+                buttonPause.Enabled = true;
+                buttonStop.Enabled = true;
+                buttonOpen.Enabled = false;
+                buttonPlay.Enabled = false;
+            }
+        }
+
+        private void PlayFileForm_Load(object sender, EventArgs e)
+        {
+            if (Bass.BASS_Init(-1, 44100, BASSInit.BASS_DEVICE_DEFAULT, Handle))
+            {
+                fileName = String.Empty;
+                streamHandle = 0;
+                waveStreamHandle = 0;
+                fileStream = null;
+                waveForm = null;
+                soundStream = new BASS_FILEPROCS(
+                    new FILECLOSEPROC(closeFile),
+                    new FILELENPROC(getFileLen),
+                    new FILEREADPROC(readFile),
+                    new FILESEEKPROC(setSeekCur)
+                );
+            }
+        }
+
+        private bool setSeekCur(long offset, IntPtr user)
+        {
+            if (fileStream == null) return false;
+            try
+            {
+                fileStream.Seek(offset, SeekOrigin.Begin);
+                return true;
+            }
+            catch (IOException ex)
+            {
+                return false;
+            }
+        }
+
+        private long getFileLen(IntPtr user)
+        {
+            if (fileStream == null) return 0;
+            return fileStream.Length;
+        }
+
+        private int readFile(IntPtr buffer, int length, IntPtr user)
+        {
+            if (fileStream == null) return 0;
+            int bytesRead = 0;
+            try
+            {
+                byte[] data = new byte[length];
+                bytesRead = fileStream.Read(data, 0, length);
+                Marshal.Copy(data, 0, buffer, bytesRead);
+            }
+            catch (IOException ex)
+            {
+                bytesRead = 0;
+            }
+            return bytesRead;
+        }
+
+        private void closeFile(IntPtr user)
+        {
+            if (fileStream == null) return;
+            fileStream.Close();
+        }
+    }
+}
